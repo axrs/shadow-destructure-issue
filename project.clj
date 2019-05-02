a3d9e50e5ea89f0667f51800e179161fc67de98d
Alexander Scott <xander@cadcoder.com>
2019-05-02 22:07:18 +1000

    Test with cljsbuild

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject lein-cljsbuild-example "1.2.3"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.520"]]
+  :plugins [[lein-cljsbuild "1.1.7"]]
+  :cljsbuild {:builds [{:source-paths ["test"]
+                        :compiler {:output-to "resources/public/unit-test.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]})
